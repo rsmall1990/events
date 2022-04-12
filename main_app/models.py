baaddc2c65d1ebfe4e7138e19761c2af5f8e48c2
Ryan Small <ryan.small@feapc.com>
2022-04-12 17:27:53 -0400

    wip: adding Update and Delete CBVs

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Event(models.Model):
@@ -9,3 +10,6 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'event_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from .models import Event
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 
 # Create your views here.
 def home(request):
@@ -15,3 +18,15 @@ def events_index(request):
 def events_detail(request, event_id):
     event = Event.objects.get(id=event_id)
     return render(request, 'events/detail.html', {'event': event})
+
+class EventCreate(CreateView):
+    model = Event
+    fields = '__all__'
+
+class EventUpdate(UpdateView):
+    model = Event
+    fields = '__all__'
+
+class EventDelete(DeleteView):
+    model = Event
+    success_url = '/events/'
