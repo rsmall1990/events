ace9cba3f465ab8c3fde2495c740ba7de8dcfc85
Ryan Small <ryan.small@feapc.com>
2022-04-12 16:58:58 -0400

    wip: adding events CBV

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('events/', views.events_index, name='index'),
     path('events/<int:event_id>/', views.events_detail, name='detail'),
-
+    path('events/create/', views.EventCreate.as_view(), name='events_create')
 ]
