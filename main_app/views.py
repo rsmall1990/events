a8adf3062adcb62da3bdb86d60a5ddb7b5302a81
Ryan Small <ryan.small@feapc.com>
2022-04-12 20:25:34 -0400

    wip: adding authentication

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,13 @@
 from .models import Event
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
@@ -11,22 +16,43 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def events_index(request):
-    events = Event.objects.all()
+    events = Event.objects.filter(user = request.user)
     return render(request, 'events/index.html', {'events': events})
 
+@login_required
 def events_detail(request, event_id):
     event = Event.objects.get(id=event_id)
     return render(request, 'events/detail.html', {'event': event})
 
-class EventCreate(CreateView):
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - please try again'
+    form = UserCreationForm()
+    context = { 'form': form, 'error_message': error_message }
+    return render(request, 'registration/signup.html', context)
+
+
+class EventCreate(LoginRequiredMixin, CreateView):
     model = Event
     fields = '__all__'
 
-class EventUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class EventUpdate(LoginRequiredMixin, UpdateView):
     model = Event
     fields = '__all__'
 
-class EventDelete(DeleteView):
+class EventDelete(LoginRequiredMixin, DeleteView):
     model = Event
     success_url = '/events/'
